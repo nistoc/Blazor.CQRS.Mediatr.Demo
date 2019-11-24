d1b0af5ba8b32cbd070a112aecd0fc1517afe2a8
Nikita Temnikov <nistoc@gmail.com>
2019-11-24 15:59:31 +0300

    the End up with SelfContainedExample #SelfContainedExample

--- BlazorSolution/BlazorApp/Pages/SelfContainedExample.razor.cs
+++ BlazorSolution/BlazorApp/Pages/SelfContainedExample.razor.cs
@@ -89,6 +89,26 @@ namespace BlazorApp.Pages
             }
         }
 
+        public class VerifyAnswerCommandHandler : IRequestHandler<VerifyAnswerCommand, Boolean>
+        {
+            public async Task<bool> Handle(VerifyAnswerCommand request, CancellationToken cancellationToken)
+            {
+                await Task.Delay(900);
+
+                switch (request.Role)
+                {
+                    case "chemist":
+                        return request.Name == "Mendeleev";
+                    case "russian doll":
+                        return request.Name == "Matreshka";
+                    case "philosopher":
+                        return request.Name == "Dostoevskii";
+                }
+
+                return false;
+            }
+        }
+
         public class ViewModel
         {
 
